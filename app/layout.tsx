80879fe76687b36e6304bddb413ef49ae9bd821d
NNQA <anhla12h@gmail.com>
2026-08-01 17:21:39 +0700

    refactor: update application layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google"
 
 import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/shared/components/theme-provider"
+import { cn } from "@/shared/lib/utils";
 import { Toaster } from "sonner";
 
 const geist = Geist({ subsets: ['latin'], variable: '--font-sans' })
