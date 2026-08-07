22a7af4393d17428cc76a731ee2946efa7a41afd
NNQA <anhla12h@gmail.com>
2026-08-07 12:15:58 +0700

    chore: initialize domain-driven project structure

--- domains/user/components/admin/standard-user/PageStandardUser.tsx
+++ domains/user/components/admin/standard-user/PageStandardUser.tsx
@@ -6,30 +6,22 @@ import { Card, CardContent, CardHeader } from '@/shared/components/ui/card'
 import { IconDownload, IconPlus } from '@tabler/icons-react'
 import React from 'react'
 import TableStandardUser from './table-manage-user'
+import { Label } from '@/shared/components/ui/label'
+import { Select } from '@/shared/components/ui/select'
 
 function PageStandardUser() {
     function onAddNew() { }
     return (
-        <div>
-            <div className="mb-4.5 flex flex-wrap items-end justify-between gap-3.5">
-                <div>
-                    <div className="text-[22px] font-extrabold tracking-[-0.01em]">User Permission</div>
-                    <div className="mt-0.75 text-[13.5px] text-muted-foreground">Control what each staff role can see and do across the store admin</div>
-                </div>
-                <div className='space-x-2.5'>
-                    <Button variant='outline' className="h-10 px-4 font-bold" onClick={onAddNew}>
-                        <IconDownload className='size-4' />
-                        Export
-                    </Button>
-                    <Button className="h-10 px-4 font-bold" onClick={onAddNew}>
-                        <IconPlus className='size-4' />
-                        Add user
-                    </Button>
-                </div>
-            </div>
+        <div className='flex items-center justify-between px-4 lg:px-6'>
+            <Label htmlFor='view-selector' className='sr-only'>
+                View
+            </Label>
+            <Select defaultValue={ }>
+
+            </Select>
             <Card>
-                <CardHeader className='flex items-end justify-between'>
-                    <SearchForm placeholdertext='Search standard user...' />
+                <CardHeader className='flex items-end justify-between border-b border-border'>
+                    <SearchForm placeholdertext='Search standard user...' className='p-2' />
                     <div className='space-x-2.5'>
                         <Button variant='outline' onClick={onAddNew}>
                             <IconDownload className='size-4' />
